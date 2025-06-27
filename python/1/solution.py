b989cd29a6642a65305048c2eba8b3287842d9d6
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-06-26 21:08:55 -0500

    1

---
+++ python/1/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        leftCounter = 0
+        
+        while leftCounter < len(nums) - 1:
+            rightCounter = leftCounter + 1
+            
+            while rightCounter < len(nums):
+                if nums[leftCounter] + nums[rightCounter] == target:
+                    return [leftCounter, rightCounter]
+                rightCounter += 1
+            
+            leftCounter += 1
